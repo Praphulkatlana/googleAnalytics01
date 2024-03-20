bd00a82026b169fc559b369803b201b66e412da5
Praphul katlana <praphul.katlana@nagarro.com>
2024-03-20 14:26:19 +0530

    GA lib added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Child from './Child';
+import ReactGA from "react-ga";
 
+const trackin_ID="G-1HFMKCP63G";
+ReactGA.initialize(trackin_ID)
 function App() {
   return (
     <div className="App">
--- src/Child.js
+++ src/Child.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Child.css";
+import ReactGA from "react-ga";
 
 const Child = () => {
+    React.useEffect(()=>{
+        ReactGA.pageview(window.location.pathname)
+    },[])
+
+    const addToWishList=(name)=>{
+        alert(" wishlist tracked!!! ")
+        ReactGA.event({
+            category:name,
+            action:"test action",
+            label:"test label"
+        })
+    }
   return (
     <div>
       <h1>Title</h1>
       <div id="contain">
         <button onClick={()=>alert("Add to cart")}>Add to cart</button>
-        <button onClick={()=>alert("Add to wishlist")}>Add to wishlist</button>
+        <button onClick={addToWishList}>Add to wishlist</button>
         <button onClick={()=>alert("contact us")}>contact us</button>
         <button onClick={()=>alert("know more")}>know more</button>
       </div>
